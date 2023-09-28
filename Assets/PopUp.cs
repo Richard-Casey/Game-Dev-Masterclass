8adfcb2c4d543c773a7bd9bec9f5d4ed820c5c43
Adam-Bakerr <s225170@uos.ac.uk>
2023-09-28 19:23:17 +0100

    Began Work On Toon Added Pop Up Start

---
+++ Assets/PopUp.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Adds A Pop Up To The Attached Object
+/// </summary>
+public class PopUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
