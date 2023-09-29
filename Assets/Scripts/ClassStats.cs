33a51cc79a92eaa2c4be1ef12ce5638d8ed85b77
Adam-Bakerr <s225170@uos.ac.uk>
2023-09-29 19:26:49 +0100

    Added stroke of a stat sytem begining

--- Assets/Scripts/ClassStats.cs
+++ Assets/Scripts/ClassStats.cs
@@ -1,47 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClassStats : MonoBehaviour
 {
-    public struct stat
+    public enum ClassStatTypes
     {
-        private string name;
-        private float value;
-        private float multiplier;
-        private float count;
+        Melee,
+        Ranged,
+        Count
+    }
 
-        public float Value() => value * multiplier;
+    public enum StatNames
+    {
+        AttackDamage,
+        RangedDamage,
+        Agility,
+        ReloadSpeed,
+        Range
+    }
+
+    public class stat
+    {
+        ClassStatTypes classtype;
+        StatNames name;
+        public float value;
+        float multiplier;
+        int amount;
+
         public float Multiplier() => multiplier;
-        public string Name() => name;
+        public ClassStatTypes Type() => classtype;
+        public int Amount() => amount;
+
+        Func<int,int,float> CalculationMethod;
 
         public void Multi(float value) => multiplier = value;
         public void AddMulti(float value) => multiplier += value;
-        public void Value(float value) => value = value;
-        public void Name(string value) => name = value;
+
+        public void AddAmount(int count)
+        {
+            float Output = (float)CalculationMethod.Invoke(count,amount);
+            amount += count;
+            value += Output;
+        }
+
+        public void Type(ClassStatTypes value) => classtype = value;
+        public void Name(StatNames value) => name = value;
+
+        public stat(ClassStatTypes type,StatNames name, float BaseValue, float BaseMultiplier, Func<int,int,float> CalculationMethod)
+        {
+            this.classtype = type;
+            this.name = name;
+            this.value = BaseValue;
+            this.multiplier = BaseMultiplier;
+            this.amount = 0;
+            this.CalculationMethod = CalculationMethod;
+
+        }
 
     }
 
-    Dictionary<string,stat> Stats = new Dictionary<string, stat>();
+    List<stat> Stats = new List<stat>();
 
-    public stat? GetStat(string name)
+    public void Start()
     {
-        if(Stats.TryGetValue(name, out stat Found)) return Found;
-        return null;
+        InitStats();
     }
 
-    public void SetStat(string name, stat newStat)
+
+    private void InitStats()
     {
-        Stats[name] = newStat;
+
+        //Adds a stat with custom calculation function -> idea behind this is reusablility and fuuture balance
+        float AttackSum(int i1,int i2) { return (i1 * (1.2f + 1f / MathF.Max(1, i2))); }
+        Func<int, int, float> AttackDamageCalculation = AttackSum;
+        Stats.Add(new stat(ClassStatTypes.Melee, StatNames.AttackDamage, 1, 1, AttackDamageCalculation));
+      
+        
+        Stats[0].AddAmount(1);
     }
 
-    public float GetValue(string name)
-    {
-        return Stats[name].Value();
-    }
 
-    public void AddMultiplier(string name, float valueToAdd)
-    {
-        Stats[name].AddMulti(valueToAdd);
-    }
 }
