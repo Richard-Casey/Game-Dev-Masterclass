f8730ff3e0276ac04b9199fee0e1cc01151bb484
Richard Casey <91665484+Richard-Casey@users.noreply.github.com>
2023-09-29 13:10:51 +0100

    Added Information Scene
    Added player assignment information screen as prelude to game - also added code to run in preview of game scene if character assignment screen had not assigned relevent information

--- Assets/Scripts/PlayerAssignment.cs
+++ Assets/Scripts/PlayerAssignment.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using TMPro;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerAssignment : MonoBehaviour
 {
@@ -25,31 +26,51 @@ public class PlayerAssignment : MonoBehaviour
     public TextMeshProUGUI classText;
     public TextMeshProUGUI roleText;
 
-    // These list keep track of the roles and classes that have already been assigned to avoid duplicates
-    private static List<PlayerClass> assignedClasses = new List<PlayerClass>();
-    private static List<PlayerRole> assignedRoles = new List<PlayerRole>();
-
+    
     void Start()
     {
-        AssignRandomClassAndRole();
-    }
+        if (GameManager.Instance == null)
+        {
+            Debug.Log("GameManager is not initialised. Creating one now.");
+            GameObject GameManager = new GameObject("GameManager");
+            GameManager.AddComponent<GameManager>();
+        }
 
-    void AssignRandomClassAndRole()
-    {
-        //Assign a random class that hasnt been assigned yet
-        List<PlayerClass> availibleClasses = new List<PlayerClass>((PlayerClass[])System.Enum.GetValues(typeof(PlayerClass)));
-        availibleClasses.RemoveAll(item => assignedClasses.Contains(item));
-        playerClass = availibleClasses[Random.Range(0, availibleClasses.Count)];
-        assignedClasses.Add(playerClass);
-        
+        if (!GameManager.Instance.isClassAndRoleAssigned)
+        {
 
-        // Assign a random role
-        List<PlayerRole> availibleRoles = new List<PlayerRole>((PlayerRole[])System.Enum.GetValues(typeof(PlayerRole)));
-        availibleRoles.RemoveAll(item => assignedRoles.Contains(item));
-        playerRole = availibleRoles[Random.Range(0, availibleRoles.Count)];
-        assignedRoles.Add(playerRole);
+            GameManager.Instance.AssignClassAndRole();
+        }
+
+        playerClass = GameManager.Instance.playerClass;
+        playerRole = GameManager.Instance.playerRole;
 
         classText.text = "Class: " + playerClass.ToString();
         roleText.text = "Role: " + playerRole.ToString();
+        
     }
+
+
+    //void AssignRandomClassAndRole()
+    //{
+    //    Debug.Log("AssignRandomClassAndRole called in PlayerAssignment");
+
+    //    //Assign a random class that hasnt been assigned yet
+    //    List<PlayerClass> availibleClasses = new List<PlayerClass>((PlayerClass[])System.Enum.GetValues(typeof(PlayerClass)));
+    //    availibleClasses.RemoveAll(item => assignedClasses.Contains(item));
+    //    playerClass = availibleClasses[Random.Range(0, availibleClasses.Count)];
+    //    assignedClasses.Add(playerClass);
+        
+
+    //    // Assign a random role
+    //    List<PlayerRole> availibleRoles = new List<PlayerRole>((PlayerRole[])System.Enum.GetValues(typeof(PlayerRole)));
+    //    availibleRoles.RemoveAll(item => assignedRoles.Contains(item));
+    //    playerRole = availibleRoles[Random.Range(0, availibleRoles.Count)];
+    //    assignedRoles.Add(playerRole);
+
+    //    GameManager.Instance.AssignClassAndRole(playerClass, playerRole);
+
+    //    classText.text = "Class: " + playerClass.ToString();
+    //    roleText.text = "Role: " + playerRole.ToString();
+    //}
 }
