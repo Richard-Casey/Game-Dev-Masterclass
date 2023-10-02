4d55265dcc256d010ad90e09c0a9359e562a4860
Adam-Bakerr <s225170@uos.ac.uk>
2023-10-02 18:56:15 +0100

    Changes

--- Assets/Scripts/Enemy/Health.cs
+++ Assets/Scripts/Enemy/Health.cs
@@ -24,4 +24,15 @@ public class Health : MonoBehaviour, IDamagable
             OnDeath?.Invoke(transform.position);
         }
     }
+
+
+    void Start()
+    {
+        OnDeath += Death;
+    }
+
+    void Death(Vector3 DeathPosition)
+    {
+
+    }
 }
